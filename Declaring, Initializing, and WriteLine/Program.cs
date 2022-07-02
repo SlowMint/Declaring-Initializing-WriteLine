77aa13b0dd550df6b5fb37aabbf4fd5849a524cf
SlowMint <individualmotiv@gmail.com>
2022-07-02 15:54:55 -0500

    initial commit

---
+++ Declaring, Initializing, and WriteLine/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+string dogName = "Sol";
+int dogAge = 1;
+char dognameFirstletter = 'S';
+bool answer = true;
+double otherDogage = 1.5;
+decimal ageGap = .66m;
+Console.WriteLine($"My dog's name is {dogName} spelled with an {dognameFirstletter}, he is {dogAge} year old. My other dog Sirius is {otherDogage} years old.");
+Console.WriteLine($"This would make {dogName} approximately {ageGap} of the Sirius' total age correct? {answer}.");
+
+
+ 
+
+
+
+
+
+
+
+
